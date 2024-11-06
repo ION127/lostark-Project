650d97fcc1a30dbe872f024b4cb1c14e54369af9
ION127 <ion011227@gmail.com>
2024-11-06 09:57:37 +0900

    first

---
+++ API/userdata.py
@@ -0,0 +1,12 @@
+import requests
+def request_Spec(name):
+    print()
+
+
+if __name__ == "__main__":
+    # 이 파일을 직접 실행했을 때만 호출되는 코드
+    print("GET 요청 테스트:")
+    request_Spec()
+
+    print("\nPOST 요청 테스트:")
+    request_refinedata()
